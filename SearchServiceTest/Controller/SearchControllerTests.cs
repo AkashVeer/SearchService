07b370d26ceff07be8c0a5a7fd6ed49221539518
AkashVeer <akashveer98@gmail.com>
2022-11-26 18:22:35 +0100

    Added UT

---
+++ SearchServiceTest/Controller/SearchControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SearchServiceLSM.Controllers;
+using SearchServiceLSM.Models;
+using SearchServiceLSM.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchServiceTest.Controller
+{
+    internal class SearchControllerTests
+    {
+        private Mock<ISearchService> searchServiceMock;
+
+        [Test]
+        public async Task SearchController_Search_ShouldBeSuccessFul()
+        {
+            searchServiceMock = new Mock<ISearchService>();
+
+            searchServiceMock.Setup(x => x.Search(It.IsAny<string>())).ReturnsAsync("1");
+
+            var controller = new SearchController(searchServiceMock.Object);
+
+            var result = (await controller.Search("entry")) as OkObjectResult;
+
+            Assert.AreEqual(StatusCodes.Status200OK, result.StatusCode);
+            Assert.AreEqual("1", result.Value.ToString());
+        }
+    }
+}
---
+++ SearchServiceTest/Service/SearchServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using SearchServiceLSM.Controllers;
+using SearchServiceLSM.Models;
+using SearchServiceLSM.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchServiceTest.Service
+{
+    internal class SearchServiceTests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public async Task SearchService_Search_ShouldBeSuccessful()
+        {
+            var searchService = new SearchService();
+
+            var result = await searchService.Search("Head Office");
+
+            Assert.IsNotEmpty(result);
+
+        }
+    }
+}
