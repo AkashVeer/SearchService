7b9c69e985adf4f4511a32c30973148cd5e99cbc
AkashVeer <akashveer98@gmail.com>
2022-11-26 17:17:03 +0100

    Added controller

---
+++ SearchService/Controllers/SearchController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SearchServiceLSM.Service;
+
+namespace SearchServiceLSM.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        private readonly ISearchService searchService;
+
+        public SearchController(ISearchService searchService)
+        {
+            this.searchService = searchService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Search([FromQuery] string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return BadRequest("Enter valid search term");
+            }
+
+            var result = await searchService.Search(text);
+            return Ok(result);
+        }
+    }
+}
